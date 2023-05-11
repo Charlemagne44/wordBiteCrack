e9a6664b065f9a7e5ecbe777a09f9c6b53ca46d6
Charlemagne44 <collinhay44@gmail.com>
2023-05-11 16:24:18 -0400

    backtrack horizontal x horizontal

--- main.go
+++ main.go
@@ -15,6 +15,14 @@ type Game struct {
 	Trie             trie.Trie
 	HorizontalChunks []string
 	VerticalChunks   []string
+	SingleChunks     []string
+	ValidWords       []string
+}
+
+func (g *Game) LoadTestWords() {
+	g.VerticalChunks = []string{"ta", "ng"}
+	g.HorizontalChunks = []string{"se", "fo", "iv"}
+	g.SingleChunks = []string{"i", "l", "b", "a", "c"}
 }
 
 func (g *Game) LoadWords() {
@@ -57,6 +65,66 @@ func (g *Game) LoadWords() {
 	}
 }
 
+func (g *Game) Backtrack(chunk string, remainingHorizontalChunks, remainingVerticalChunks,
+	remainingSingleChunks []string, chunk_orientation rune) {
+
+	// if word is horizontal
+	if chunk_orientation == 'h' {
+		// try all horizontal chunks before the beginning and end
+		for _, horiz_chunk := range g.HorizontalChunks {
+
+			// insert chunk before and check validity
+			new_word := horiz_chunk + chunk
+			// append a valid scoring word to the games valid words
+			if g.Trie.Search(new_word) && len(new_word) >= 3 {
+				g.ValidWords = append(g.ValidWords, new_word)
+			}
+			// is its valid, and more could be constructed -> recurse
+			if g.Trie.ValidPath(new_word) {
+				// create a new limited list of horizontal chunks excluding the one used and recurse
+				g.Backtrack(new_word, remove(remainingHorizontalChunks, horiz_chunk),
+					remainingVerticalChunks, remainingSingleChunks, 'h')
+			}
+
+			// insert chunk after and check validity
+			new_word = chunk + horiz_chunk
+			// append a valid scoring word to the games valid words
+			if g.Trie.Search(new_word) && len(new_word) >= 3 {
+				g.ValidWords = append(g.ValidWords, new_word)
+			}
+			// if its valid, and more could be constructed -> recurse
+			if g.Trie.ValidPath(new_word) {
+				// create a new limited list of horizontal chunks excluding the one used and recurse
+				g.Backtrack(new_word, remove(remainingHorizontalChunks, horiz_chunk),
+					remainingVerticalChunks, remainingSingleChunks, 'h')
+			}
+		}
+
+		// try all vertical chunks before and after each letter
+
+		// try all single chunks before and after chunk, and before and after each letter
+
+	} else { // if word is vertical
+
+		// try all vertical chunks before the beginning and the end
+
+		// try all horizontal chunks before and after each letter
+
+		// try all single chunks before and after chunk, and before and after each letter
+
+	}
+
+}
+
+func remove(s []string, r string) []string {
+	for i, v := range s {
+		if v == r {
+			return append(s[:i], s[i+1:]...)
+		}
+	}
+	return s
+}
+
 func main() {
 	// load in the english dict into a trie
 	data, _ := f.ReadFile("resources/scrabble.json")
@@ -79,13 +147,36 @@ func main() {
 		Trie:             *trie,
 		VerticalChunks:   make([]string, 0),
 		HorizontalChunks: make([]string, 0),
+		SingleChunks:     make([]string, 0),
+		ValidWords:       make([]string, 0),
 	}
 
 	// gathers user input for vertical and horizontal chunks and load them into the game object
-	game.LoadWords()
+	// game.LoadWords()
+	game.LoadTestWords()
 	fmt.Printf("Horizontal chunks: %v\n", game.HorizontalChunks)
 	fmt.Printf("Vertical chunks: %v\n", game.VerticalChunks)
+	fmt.Printf("Single chunks: %v\n", game.SingleChunks)
 
-	// backgrack to find longest and highest scoring combinations
+	// create the initial copy of the chunksthat can be removed during recursion
+	remainingHorizontalChunks := game.HorizontalChunks
+	remainingVerticalChunks := game.VerticalChunks
+	remainingSingleChunks := game.SingleChunks
+
+	// backtrack to find longest and highest scoring combinations
+	for _, horizontal_chunk := range game.HorizontalChunks {
+		game.Backtrack(horizontal_chunk, remove(game.HorizontalChunks, horizontal_chunk),
+			remainingVerticalChunks, remainingSingleChunks, 'h')
+	}
+
+	for _, vertical_chunk := range game.VerticalChunks {
+		game.Backtrack(vertical_chunk, remainingHorizontalChunks,
+			remove(game.VerticalChunks, vertical_chunk), remainingSingleChunks, 'v')
+	}
+
+	fmt.Printf("Valid words: %v\n", game.ValidWords)
+
+	// fmt.Println(trie.Search("sese"))
+	// fmt.Println(trie.ValidPath("ashen"))
 
 }
